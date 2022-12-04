ead5bf6053e88118e81cc35a034fb8b0bd95f6f0
Andres <af.orozcog@uniandes.edu.co>
2022-12-04 08:50:42 -0500

    add capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -51,7 +51,7 @@
 # ------------------------------------
 server "54.237.128.166",
   user: "ec2-user",
-  roles: %w{web app},
+  roles: %w{web db app},
   ssh_options: {
     user: "ec2-user", # overrides user setting above
     keys: %w(/home/andres/.ssh/dic_sample.pem),
